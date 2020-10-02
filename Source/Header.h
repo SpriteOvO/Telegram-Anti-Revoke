5848e76237e6fe18e7e601616fc943f9ceb0f310
Sprite <SpriteOvO@gmail.com>
2020-10-02 15:47:39 +0800

    Version 0.2.5

--- Source/Header.h
+++ Source/Header.h
@@ -18,6 +18,6 @@
 using namespace std;
 
 // Current version
-#define AR_VERSION        "0.2.4"
+#define AR_VERSION        "0.2.5"
 #define AR_URL_ISSUES     "https://github.com/SpriteOvO/Telegram-Anti-Revoke/issues"
 #define AR_URL_RELEASE    "/repos/SpriteOvO/Telegram-Anti-Revoke/releases/latest"
