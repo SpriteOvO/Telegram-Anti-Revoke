89dd8d610d8c2e79a7f5f8d6cd8506879b39342d
Sprite <SpriteOvO@gmail.com>
2020-11-09 14:50:47 +0800

    Move config macros to Config.h

---
+++ Source/Config.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+// Current version
+#define AR_VERSION           "0.2.6"
+
+#define AR_OWNER_REPO        "SpriteOvO/Telegram-Anti-Revoke"
+#define AR_REPO_URL          "https://github.com/" AR_OWNER_REPO
+#define AR_ISSUES_URL        AR_REPO_URL "/issues"
+#define AR_LATEST_REQUEST    "/repos/" AR_OWNER_REPO "/releases/latest"
--- Source/Header.h
+++ Source/Header.h
@@ -17,10 +17,5 @@
 
 using namespace std;
 
-// Current version
-#define AR_VERSION           "0.2.6"
+#include "Config.h"
 
-#define AR_OWNER_REPO        "SpriteOvO/Telegram-Anti-Revoke"
-#define AR_REPO_URL          "https://github.com/" AR_OWNER_REPO
-#define AR_ISSUES_URL        AR_REPO_URL "/issues"
-#define AR_LATEST_REQUEST    "/repos/" AR_OWNER_REPO "/releases/latest"
