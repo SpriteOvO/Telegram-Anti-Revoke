391d5da5f658164736e3699ecf5f009ba9b32929
Sprite <SpriteOvO@gmail.com>
2020-12-23 21:15:54 +0800

    Add a new block to the version info resource

--- Source/Config.h
+++ Source/Config.h
@@ -13,7 +13,15 @@
 #define AR_VERSION_RSRC_NUM  AR_VERSION_MAJOR, AR_VERSION_MINOR, AR_VERSION_BUILD, 0
 #define AR_VERSION_RSRC_STR  AR_VERSION_STRING ".0"
 
+#if defined OS_WIN7
+# define AR_PLATFORM_OS_STR  "Win7"
+#elif defined OS_WIN10
+# define AR_PLATFORM_OS_STR  "Win10"
+#endif
+
 #define AR_OWNER_REPO        "SpriteOvO/Telegram-Anti-Revoke"
 #define AR_REPO_URL          "https://github.com/" AR_OWNER_REPO
+#define AR_REPO_URL_SHORT    "github.com/" AR_OWNER_REPO
 #define AR_ISSUES_URL        AR_REPO_URL "/issues"
 #define AR_LATEST_REQUEST    "/repos/" AR_OWNER_REPO "/releases/latest"
+#define AR_COPYRIGHT         "Copyright (C) 2019-2020 SpriteOvO. All rights reserved."
