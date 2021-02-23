752328a28402aac5ab8a35112a604b12911d9243
Sprite <SpriteOvO@gmail.com>
2021-02-24 04:58:59 +0800

    Support to v2.6.0

--- Source/IRuntime.cpp
+++ Source/IRuntime.cpp
@@ -44,15 +44,25 @@ bool IRuntime::InitFixedData()
         _Data.Offset.SignedTimeText = 0x14;
         _Data.Offset.HistoryPeer = 0x7C;
     }
-    // ver >= 2.4.1
-    else if (_FileVersion >= 2004001)
+    // ver >= 2.4.1, ver < 2.6.0
+    else if (_FileVersion >= 2004001 && _FileVersion < 2006000)
     {
         _Data.Offset.TimeText = 0x70;
         _Data.Offset.TimeWidth = 0x74;
         _Data.Offset.MainView = 0x5C;
         _Data.Offset.Media = 0x54;
         _Data.Offset.SignedTimeText = 0x10;    // changed
-        _Data.Offset.HistoryPeer = 0x7C;
+        _Data.Offset.HistoryPeer = 0x7C;       // maybe untested! (I forgot :)
+    }
+    // ver >= 2.6.0
+    else if (_FileVersion >= 2006000)
+    {
+        _Data.Offset.TimeText = 0x78;          // changed
+        _Data.Offset.TimeWidth = 0x7C;         // changed
+        _Data.Offset.MainView = 0x60;          // changed
+        _Data.Offset.Media = 0x5C;             // changed
+        _Data.Offset.SignedTimeText = 0x10;
+        _Data.Offset.HistoryPeer = 0x7C;       // untested!
     }
 
     return true;
